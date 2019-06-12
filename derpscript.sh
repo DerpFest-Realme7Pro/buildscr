506a04e5e6fa59fef5db1cf13358c64aa79f7c3e
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-06-12 11:01:22 +0530

    Sanders: Add Updater to script

--- derpscript.sh
+++ derpscript.sh
@@ -5,3 +5,4 @@ rm -rf packages/apps/Updater
 rm -rf packages/apps/Camera2
 git clone https://github.com/omnirom/android_packages_apps_SnapdragonCamera2.git -b android-9.0 packages/apps/SnapdragonCamera2
 git clone https://github.com/Xtended-Devices/android_packages_resources_devicesettings.git -b xp packages/resources/devicesettings
+git clone https://github.com/derpfest-sanders/platform_packages_apps_Updater.git -b pie packages/apps/Updater
