6cded89867790f6db5339d5addcabef855c70ba6
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-06-15 10:13:08 +0530

    Script: Change kernel to Unravel 2.0

--- derpscript.sh
+++ derpscript.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/marshmello61/derp_device_motorola_sanders.git -b aosip-eas device/motorola/sanders
 git clone https://github.com/marshmello61/vendor_motorola_sanders.git -b stable-h vendor/motorola/sanders
-git clone https://github.com/marshmello61/kernel_motorola_msm8953.git -b test-v kernel/motorola/msm8953
+git clone https://github.com/marshmello61/mayhem-kernel.git -b unravel kernel/motorola/msm8953
 rm -rf packages/apps/Updater
 rm -rf packages/apps/Camera2
 git clone https://github.com/omnirom/android_packages_apps_SnapdragonCamera2.git -b android-9.0 packages/apps/SnapdragonCamera2
