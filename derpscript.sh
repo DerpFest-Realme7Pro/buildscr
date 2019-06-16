ba3d222003825e40ef44fa4f46b89d94072fb81b
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-06-16 12:15:12 +0530

    Script: Now this will act as sync
    Now running script will act as syncing trees.
    First of all it will remove old trees and then clone new trees.
    
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>

--- derpscript.sh
+++ derpscript.sh
@@ -1,8 +1,13 @@
+rm -rf device/motorola/sanders
+rm -rf vendor/motorola/sanders
+rm -rf kernel/motorola/msm8953
 git clone https://github.com/marshmello61/derp_device_motorola_sanders.git -b aosip-eas device/motorola/sanders
 git clone https://github.com/marshmello61/vendor_motorola_sanders.git -b stable-h vendor/motorola/sanders
 git clone https://github.com/marshmello61/mayhem-kernel.git -b unravel kernel/motorola/msm8953
 rm -rf packages/apps/Updater
 rm -rf packages/apps/Camera2
+rm -rf packages/apps/SnapdragonCamera2
+rm -rf packages/resources/devicesettings
 git clone https://github.com/omnirom/android_packages_apps_SnapdragonCamera2.git -b android-9.0 packages/apps/SnapdragonCamera2
 git clone https://github.com/Xtended-Devices/android_packages_resources_devicesettings.git -b xp packages/resources/devicesettings
 git clone https://github.com/derpfest-sanders/platform_packages_apps_Updater.git -b pie packages/apps/Updater
