6b91b97384d77fa71f9c524bb5ebf7921799e8b0
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-08-12 18:59:02 +0530

    Script: remove the fork of aosip's own recovery-twrp and clone recovery
    Currently recovery-twrp is cloned in bootable/recovery, 
    which gives the exceeding size of recovery error.
    
    So remove it and clone the old recovery that was fine.
    
    
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- derp.sh
+++ derp.sh
@@ -8,6 +8,8 @@ rm -rf packages/apps/Updater
 rm -rf packages/apps/Camera2
 rm -rf packages/apps/SnapdragonCamera2
 rm -rf packages/resources/devicesettings
-git clone https://github.com/omnirom/android_packages_apps_SnapdragonCamera2.git -b android-9.0 packages/apps/SnapdragonCamera2
+rm -rf bootable/recovery
+git clone https://github.com/marshmello61/android_packages_apps_SnapdragonCamera2.git -b pie packages/apps/SnapdragonCamera2
 git clone https://github.com/Xtended-Devices/android_packages_resources_devicesettings.git -b xp packages/resources/devicesettings
 git clone https://github.com/derpfest-sanders/platform_packages_apps_Updater.git -b pie packages/apps/Updater
+git clone https://github.com/AOSiP/platform_bootable_recovery.git -b pie bootable/recovery
