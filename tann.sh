b265287051a2e60185078b66af63a61d847e17e6
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-11-01 16:56:02 +0530

    Don't forget devicesettings
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- tann.sh
+++ tann.sh
@@ -4,3 +4,5 @@ rm -rf vendor/motorola/sanders
 git clone https://github.com/ronaxdevil/vendor_motorola_sanders.git -b ten vendor/motorola/sanders
 git clone https://github.com/ronaxdevil/mayhem-kernel.git -b ten kernel/motorola/msm8953
 git clone https://github.com/marshmello61/tann.git -b d10 device/motorola/sanders
+rm -rf packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-17.0 packages/resources/devicesettings
