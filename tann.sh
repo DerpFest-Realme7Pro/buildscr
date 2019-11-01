2f38914d6c79be6e21b0d5bcd4c178e1228c4f1b
marshmello61 <ultramayur123@gmail.com>
2019-11-01 09:53:32 +0000

    Branch=10
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- tann.sh
+++ tann.sh
@@ -2,5 +2,5 @@ rm -rf device/motorola/sanders
 rm -rf kernel/motorola/msm8953
 rm -rf vendor/motorola/sanders
 git clone https://github.com/ronaxdevil/vendor_motorola_sanders.git -b ten vendor/motorola/sanders
-git clone https://github.com/ronaxdevil/mayhem-kernel.git -b kernel/motorola/msm8953
+git clone https://github.com/ronaxdevil/mayhem-kernel.git -b ten kernel/motorola/msm8953
 git clone https://github.com/marshmello61/tann.git -b d10 device/motorola/sanders
