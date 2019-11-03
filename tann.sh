e3a5452762a286c2f39ec44512aab035bc9be883
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-11-03 14:30:57 +0530

    Update tann.sh

--- tann.sh
+++ tann.sh
@@ -1,8 +1,9 @@
 rm -rf device/motorola/sanders
 rm -rf kernel/motorola/msm8953
 rm -rf vendor/motorola/sanders
-git clone https://github.com/ronaxdevil/vendor_motorola_sanders.git -b ten vendor/motorola/sanders
-git clone https://github.com/ronaxdevil/mayhem-kernel.git -b ten kernel/motorola/msm8953
-git clone https://github.com/marshmello61/tann.git -b d10 device/motorola/sanders
+git clone https://github.com/ArrowOS-Devices/android_vendor_motorola_sanders.git -b arrow-10.0 vendor/motorola/sanders
+git clone https://github.com/ArrowOS-Devices/android_kernel_motorola_sanders.git -b arrow-10.0 kernel/motorola/msm8953
+git clone https://github.com/marshmello61/tann.git -b d101 device/motorola/sanders
 rm -rf packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-17.0 packages/resources/devicesettings
+git clone https://github.com/ArrowOS/android_system_qcom -b arrow-10.0 system/qcom
