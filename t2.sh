d50b928418544f915c071b418c7b4ea18d9c199b
marshmello61 <ultramayur123@gmail.com>
2019-11-08 13:42:26 +0000

    buildscript: t2: refract to DerpFest-Sandere org
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- t2.sh
+++ t2.sh
@@ -1,8 +1,8 @@
 rm -rf device/motorola/sanders
 rm -rf kernel/motorola/msm8953
 rm -rf vendor/motorola/sanders
-git clone https://github.com/ArrowOS-Devices/android_vendor_motorola_sanders.git -b arrow-10.0 vendor/motorola/sanders
-git clone https://github.com/ArrowOS-Devices/android_kernel_motorola_sanders.git -b arrow-10.0 kernel/motorola/msm8953
+git clone https://github.com/DerpFest-Sanders/platform_vendor_motorola_sanders.git -b ten vendor/motorola/sanders
+git clone https://github.com/DerpFest-Sanders/platform_kernel_motorola_sanders.git -b ten kernel/motorola/msm8953
 git clone https://github.com/marshmello61/tann.git -b mtest device/motorola/sanders
 rm -rf packages/resources/devicesettings
 rm -rf hardware/qcom-caf/msm8996/display
