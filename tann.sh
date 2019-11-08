304af70090402258a8cc0e82843c2bd5e5a5a7c4
marshmello61 <ultramayur123@gmail.com>
2019-11-08 12:58:05 +0000

    buildscript: add Updater
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- tann.sh
+++ tann.sh
@@ -8,7 +8,9 @@ rm -rf packages/resources/devicesettings
 rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/power
 rm -rf hardware/qcom/power
+rm -rf packages/apps/Updater
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-17.0 packages/resources/devicesettings
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_display.git -b lineage-17.0-caf-msm8996 hardware/qcom-caf/msm8996/display
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom-caf/power
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom/power
+git clone https://github.com/DerpFest-Sanders/platform_packages_apps_Updater.git -b ten Updater
