6f930c57a11ad85f765c3f7ecfe4f28ab60cab3e
Mayur Varde <44891334+marshmello61@users.noreply.github.com>
2019-12-04 15:54:52 +0530

    Sanders is Official now

--- t2.sh
+++ t2.sh
@@ -3,16 +3,16 @@ rm -rf kernel/motorola/msm8953
 rm -rf vendor/motorola/sanders
 git clone https://github.com/DerpFest-Sanders/platform_vendor_motorola_sanders.git -b ten vendor/motorola/sanders
 git clone https://github.com/DerpFest-Sanders/platform_kernel_motorola_sanders.git -b ten kernel/motorola/msm8953
-git clone https://github.com/marshmello61/tann.git -b d101 device/motorola/sanders
+git clone https://github.com/marshmello61/tann.git -b rebase device/motorola/sanders
 rm -rf packages/resources/devicesettings
 rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/power
 rm -rf hardware/qcom/power
-rm -rf packages/apps/Updater
+echo "Yo boii"
 rm -rf system/qcom
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-17.0 packages/resources/devicesettings
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_display.git -b lineage-17.0-caf-msm8996 hardware/qcom-caf/msm8996/display
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom-caf/power
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom/power
-git clone https://github.com/DerpFest-Sanders/platform_packages_apps_Updater.git -b ten packages/apps/Updater
+echo "xD"
 git clone https://github.com/ArrowOS/android_system_qcom.git -b arrow-10.0 system/qcom
