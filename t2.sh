10cb79f90266625d396ff0732c73cc2545f8a860
marshmello61 <ultramayur123@gmail.com>
2020-01-12 03:18:33 +0000

    rwmove system/qcom
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- t2.sh
+++ t2.sh
@@ -9,10 +9,8 @@ rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/power
 rm -rf hardware/qcom/power
 echo "Yo boii"
-rm -rf system/qcom
 echo "ok"
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_display.git -b lineage-17.0-caf-msm8996 hardware/qcom-caf/msm8996/display
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom-caf/power
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom/power
 echo "xD"
-git clone https://github.com/ArrowOS/android_system_qcom.git -b arrow-10.0 system/qcom
