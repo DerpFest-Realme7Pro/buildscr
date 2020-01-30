2a3f70772cfff5b01bfa1300e18bde81cd400a7e
Mayur <ultramayur123@gmail.com>
2020-01-30 17:26:15 +0000

    remove display hal

--- t2.sh
+++ t2.sh
@@ -5,12 +5,10 @@ git clone https://github.com/DerpFest-Devices/vendor_motorola_sanders.git -b ten
 git clone https://github.com/ronaxdevil/parallax_kernel_sanders.git -b p-pf kernel/motorola/msm8953
 git clone https://github.com/DerpFest-Devices/device_motorola_sanders.git -b ten device/motorola/sanders
 echo "lol"
-rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/power
 rm -rf hardware/qcom/power
 echo "Yo boii"
 echo "ok"
-git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_display.git -b lineage-17.0-caf-msm8996 hardware/qcom-caf/msm8996/display
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom-caf/power
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom/power
 echo "xD"
