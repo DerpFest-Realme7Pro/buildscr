b271f8bc42f20747fc1fde5ecf6429aedfd80f9a
Mayur <ultramayur123@gmail.com>
2020-03-11 05:12:36 +0000

    add evo script
    Signed-off-by: Mayur <ultramayur123@gmail.com>

---
+++ evo.sh
@@ -0,0 +1,8 @@
+echo "lol"
+rm -rf hardware/qcom-caf/power
+rm -rf hardware/qcom/power
+echo "Yo boii"
+echo "ok"
+git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom-caf/power
+git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom/power
+echo "xD"
