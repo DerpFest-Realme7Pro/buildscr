9cc44d623064dcaec6d582d75336f4876df2481d
Mayur Varde <ultramayur123@gmail.com>
2020-03-21 21:30:32 +0530

    do not abort on errors. I repeat, do not

--- dp.sh
+++ dp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Abort on errors
-set -e
+#set -e
 
 BUILD_START=$(date +"%s")
 blue='\033[0;34m'
@@ -15,7 +15,7 @@ nocol='\033[0m'
 
 
 ## fw/base
-echo -e "$blue// Picking commits of frameworks/base //$nocol"
+echo "$blue// Picking commits of frameworks/base //$nocol"
 cd frameworks/base
 git fetch https://github.com/DerpFest-Sanders/platform_frameworks_base ten
 git cherry-pick b4b044ddf423bcf0f9780cc3ae885e95dc81cede
@@ -26,7 +26,7 @@ echo " "
 
 
 ## DerpQuest
-echo -e "$blue// Picking commits of DerpQuest //$nocol"
+echo "$blue// Picking commits of DerpQuest //$nocol"
 cd pac*/apps/DerpQuest
 git fetch https://github.com/DerpFest-Sanders/platform_packages_apps_DerpQuest ten
 git cherry-pick a1cd7e0e9f20247e494763b742daa1966d6c4f02
@@ -37,7 +37,7 @@ echo " "
 
 
 ## build/make
-echo -e "$blue// Picking commits of build //$nocol"
+echo "$blue// Picking commits of build //$nocol"
 cd build/make
 git fetch https://github.com/DerpFest-Sanders/platform_build mine
 git cherry-pick fea68116eb2232e376d6a7d90af177b1f6bc7f0f
@@ -48,7 +48,7 @@ echo " "
 
 
 ## vendor/overlay
-echo -e "$blue// Picking commits of vendor/overlay //$nocol"
+echo "$blue// Picking commits of vendor/overlay //$nocol"
 cd vendor/overlay
 git fetch https://github.com/DerpFest-Sanders/platform_vendor_overlay ten
 git cherry-pick 07cc4691d1f8e18bb48e256caa1da444b3c0ccb7
@@ -59,7 +59,7 @@ echo " "
 
 
 ## Settings
-echo -e "$blue// Picking commits of Settings //$nocol"
+echo "$blue// Picking commits of Settings //$nocol"
 cd pac*/apps/Settings
 git fetch https://github.com/DerpFest-Sanders/platform_packages_apps_Settings ten
 git cherry-pick 185d989450e8a5da0ca164e3e08f3bdce5fc0d75
@@ -71,7 +71,7 @@ echo " "
 
 
 ## vendor/pixelstyle
-echo -e "$blue// Picking commits of vendor/pixelstyle //$nocol"
+echo "$blue// Picking commits of vendor/pixelstyle //$nocol"
 cd vendor/pixelstyle
 git fetch https://github.com/DerpFest-Sanders/platform_vendor_pixelstyle ten
 git cherry-pick 92faa55d29ccb29b147cab1e0a42b99b2929b002
@@ -82,7 +82,7 @@ echo " "
 
 
 ## vendor/aosip
-echo -e "$blue// Picking commits of vendor/aosip //$nocol"
+echo "$blue// Picking commits of vendor/aosip //$nocol"
 cd vendor/aosip
 git fetch https://github.com/DerpFest-Sanders/platform_vendor_aosip ten
 git cherry-pick da46abff6be7fdba01a5568d572a576a8f428fab
@@ -93,7 +93,7 @@ echo " "
 
 
 ## OmniStyle
-echo -e "$blue// Picking commits of OmniStyle //$nocol"
+echo "$blue// Picking commits of OmniStyle //$nocol"
 cd pac*/apps/OmniStyle
 git fetch https://github.com/DerpFest-Sanders/platform_packages_apps_OmniStyle ten
 git cherry-pick 568431bd327180c0c7a9147a649c0bf6af270b73
