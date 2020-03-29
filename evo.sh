180803bb15285420cc9693e61802ca260392d6f3
Mayur Varde <ultramayur123@gmail.com>
2020-03-29 22:27:00 +0530

    Add kernel to evo.sh

--- evo.sh
+++ evo.sh
@@ -1,8 +1,10 @@
 echo "lol"
 rm -rf hardware/qcom-caf/power
 rm -rf hardware/qcom/power
+rm -rf kernel/motorola/msm8953
 echo "Yo boii"
 echo "ok"
+git clone https://github.com/marshmello61/parallax_kernel_sanders.git -b p-pf kernel/motorola/msm8953
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom-caf/power
 git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom/power
 echo "xD"
