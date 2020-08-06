423cd2c4d83a0a226c67ecb67c50ec31babe595a
Mayur <ultramayur123@gmail.com>
2020-08-06 02:20:56 +0000

    update evo.sh
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- evo.sh
+++ evo.sh
@@ -1,10 +1,32 @@
-echo "lol"
-rm -rf hardware/qcom-caf/power
-rm -rf hardware/qcom/power
-rm -rf kernel/motorola/msm8953
-echo "Yo boii"
-echo "ok"
-git clone https://github.com/marshmello61/parallax_kernel_sanders.git -b p-pf kernel/motorola/msm8953
-git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom-caf/power
-git clone https://github.com/DerpFest-Sanders/android_hardware_qcom_power.git -b lineage-16.0 hardware/qcom/power
-echo "xD"
+# Init
+if ! [ "${init}" = "l" ]; then
+    if ! [ "${init}" = "h" ]; then
+        init=""
+    fi
+fi
+
+if [[ "${init}" == "" ]]; then
+    echo -n "gitlab or github?[l/h]: "
+    read init
+fi
+
+if [[ "${init}" == "l" ]]; then
+    repo init -u https://gitlab.com/EvoX/manifest -b ten
+elif [[ "${init}" == "h" ]]; then
+    repo init -u https://github.com/Evolution-X/manifest -b ten
+else
+    echo "You were supposed to l/h"
+    echo "Rerunning the script"
+    sleep 2
+    . buildscr/evo.sh
+fi
+
+# Sync
+repo sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --force-sync -j$(nproc --all) --quiet
+
+# system/qcom
+if [ -e system/qcom ]; then
+   echo "system/qcom exists"
+else
+   git clone https://github.com/ArrowOS/android_system_qcom -b arrow-10.0 system/qcom
+fi
