0e33f0d95d54b91c85933b6a41df9ec098b369ea
Mayur Varde <ultramayur123@gmail.com>
2020-10-10 19:36:18 +0530

    update for dp11

--- dp11.sh
+++ dp11.sh
@@ -22,6 +22,14 @@ else
    git clone https://github.com/LineageOS/android_system_tools_dtbtool -b lineage-17.1 system/tools/dtbtool
 fi
 
+# hardware/qcom-caf/wlan
+echo "// Replacing hardware/qcom-caf/wlan //"
+sleep 1
+rm -rf hardware/qcom-caf/wlan
+git clone https://github.com/DerpFest-Sanders/platform_hardware_qcom-caf_wlan -b 11 hardware/qcom-caf/wlan
+echo "Done"
+sleep 1
+
 # system/tools/mkbootimg
 echo "// Adding --dt support in system/tools/mkbootimg //"
 sleep 1
@@ -36,4 +44,12 @@ sleep 1
 cd system/sepolicy
 git fetch https://github.com/DerpFest-Sanders/platform_system_sepolicy 11
 git cherry-pick 8ae2b97ed235424f68bd28cb9935603456fcc66b
-cd ..
+cd ../..
+
+# vendor/derp
+echo "// Removing gapps //"
+sleep 1
+cd vendor/derp
+git fetch https://github.com/DerpFest-Sanders/platform_vendor_derp 11
+git cherry-pick -s 081ee1e214cf806f0d797ee829e2afdf6ea24365
+cd ../..
